09305f22832ed797cfe05727200f3a4f4fa83611
DM1122 <34867698+DM1122@users.noreply.github.com>
2021-07-13 15:18:52 -0400

    Organized tests into subfolders by system

---
+++ tests/tests_datahandler/test_data_handler.py
@@ -0,0 +1,14 @@
+"""Test for datahandler."""
+
+# stdlib
+import logging
+
+# external
+import pytest
+
+LOG = logging.getLogger(__name__)
+
+
+@pytest.mark.skip(reason="WIP")
+def test_xyz():
+    """A test."""

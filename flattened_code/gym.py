Fix faulty accuracy calculation in Trainer

flattened_code/gym.py
@@ -155,20 +155,18 @@ class Trainer:
 
                 # metrics
                 loss_sum += loss_batch
-                predictions = torch.argmax(input=logits, dim=1, keepdim=False)
+                predictions = (logits>0.5).float() # https://discuss.pytorch.org/t/bcewithlogitsloss-and-model-accuracy-calculation/59293
                 try:  # only compute accuracy for classification tasks
-                    correct_sum += (
-                        (predictions == labels).type(torch.float).sum().item()
-                    )
+                    correct_sum += (predictions == labels).float().sum()
                 except RuntimeError:
-                    correct_sum = 0
+                    correct_sum += 0
 
                 i += 1
                 if i >= samples:
                     break
 
-        loss = loss_sum / len(dataloader)
-        acc = correct_sum / len(dataloader)
+        loss = loss_sum / (samples * 20) # 20 is sequence length
+        acc = correct_sum / (samples * 20) # 20 is sequence length
 
         return loss, acc
 
@@ -194,15 +192,16 @@ class Trainer:
             logits = self.model(inputs)
             loss_batch = self.criterion(logits, labels)
 
-        loss_train = loss_batch / self.dataloader_train.batch_size
+        loss_train = loss_batch / (self.dataloader_train.batch_size * labels.shape[1])
 
         # train accuracy
-        predictions = torch.argmax(input=logits, dim=1, keepdim=False)
+        predictions = (logits>0.5).float() # https://discuss.pytorch.org/t/bcewithlogitsloss-and-model-accuracy-calculation/59293
+
         try:  # only compute accuracy for classification tasks
-            correct_sum = (predictions == labels).type(torch.float).sum().item()
+            correct_sum = (predictions == labels).float().sum()
         except RuntimeError:
-            correct_sum = 0
-        acc_train = correct_sum / self.dataloader_train.batch_size
+            correct_sum = 0        
+        acc_train = correct_sum / (self.dataloader_train.batch_size * labels.shape[1])
 
         # test metrics
         loss_test, acc_test = self.test(dataloader=self.dataloader_test)

flattened_code/models.py
@@ -4,6 +4,9 @@
 # external
 import numpy as np
 import torch
+import logging
+
+LOG = logging.getLogger(__name__)
 
 
 class LinearParametric(torch.nn.Module):
@@ -202,7 +205,7 @@ class RNN(torch.nn.Module):
 
         """
         x, _ = self.rnn(x)
-        x = self.fc(x)
+        x = torch.sigmoid(self.fc(x))
 
         return x
 

flattened_code/rnn_data_loader.py
@@ -91,11 +91,11 @@ def read_rnn_dataloaders(features, labels, dataset_type, iterator='', batch_size
     LOG.info(f"Labels {dataset_type}set is {labels}")
 
     # read files to pandas then numpy
-    features = torch.tensor((torch.load(features))).to('cuda:0')
+    features = torch.tensor((torch.load(features)))#.to('cuda:0')
     labels = (pd.read_csv(labels, header=None)).to_numpy()
     # region dataloaders
     LOG.info("Creating dataloaders")
-    labels = torch.tensor(labels, dtype=torch.float).to('cuda:0')
+    labels = torch.tensor(labels, dtype=torch.float)#.to('cuda:0')
 
     labels=labels.squeeze()
     labels=labels.squeeze()

flattened_code/rnn_training.py
@@ -41,7 +41,7 @@ dataloader_valid= rnn_data_loader.read_rnn_dataloaders('encoded_features', 'labe
 
 # model definiton
 LOG.info("Instantiating model")
-model = models.RNN(input_size=4, hidden_size=16, num_rnn_layers=1, output_size=1).to(
+model = models.RNN(input_size=4, hidden_size=64, num_rnn_layers=1, output_size=1).to(
     device
 )
 model=model.double()
@@ -53,8 +53,8 @@ summary = torchinfo.summary(
 )
 LOG.info(f"Model:\n{summary}")
 
-optimizer = torch.optim.Adam(params=model.parameters(), lr=0.13832)
-criterion = torch.nn.MSELoss(reduction="sum")
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
+criterion = torch.nn.BCELoss(reduction="sum") # <-------------------------- TODO: change from torch.nn.MSELoss() to torch.nn.BCEWithLogitsLoss() @Eddie
 
 # training
 trainer = gym.Trainer(
@@ -66,7 +66,7 @@ trainer = gym.Trainer(
     device=device,
     logname="test_time_series",
 )
-tb = trainer.train(iterations=100)
+tb = trainer.train(iterations=500)
 tb.close()
 
 loss_valid, acc_valid = trainer.test(dataloader=dataloader_valid)